07defc1abdebd174c7654b60874665924c57865a
Sakif Arnob <sakifarnob98@gmail.com>
2024-10-18 00:21:04 +0600

    pre-final

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'Arnob',
     description:
       'Saadman Sakif Arnob, from Dhaka, Bangladesh. An DevOps enthusiast, passionate about driving automation, scalability, and efficiency.',
-    siteUrl: 'https://arnob.netlify.app', // No trailing slash allowed!
+    siteUrl: 'https://arn0b.netlify.app', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@sakifarnob98',
   },
